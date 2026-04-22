2eb02d7978113c4012ded20f8af51a354b00258f
Suhani Sharma <159052230+SuhaniSharma1309@users.noreply.github.com>
2026-04-22 14:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (15.02%) - LeetSync

---
+++ Easy/53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int result=nums[0];
+        int maxending=nums[0];
+
+        for(int i=1; i<n; i++){
+            maxending=max(maxending+nums[i], nums[i]);
+            result=max(maxending, result);
+        }
+        return result;
+    }
+};
