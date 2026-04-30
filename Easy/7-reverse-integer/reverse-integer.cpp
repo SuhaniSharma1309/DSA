36b87acb5e1954e369cae3428f78e883a5a35903
Suhani Sharma <159052230+SuhaniSharma1309@users.noreply.github.com>
2026-04-30 20:04:46 +0530

    Time: 2 ms (38.27%) | Memory: 8.6 MB (53.37%) - LeetSync

---
+++ Easy/7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+        bool isNegative=(x < 0);
+        long long num=abs((long long) x);
+        long long rev=0;
+        // int original=x;
+        while(num>0){
+            rev= num % 10 + rev*10;
+            num = num/10;
+        }
+        if (isNegative) rev= -rev;
+
+        if(rev > INT_MAX || rev < INT_MIN) return 0;
+
+        return (int)rev;
+        // return rev;
+        
+    return 0;
+    }
+    
+};
